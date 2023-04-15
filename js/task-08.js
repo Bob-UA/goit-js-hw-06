06dd812b6d2d8514501e0b822998bd92f1814e14
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-14 20:50:52 -0700

    8-task_completed

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector(".login-form");
 
+const handleSubmit = (event) => {
+    event.preventDefault();
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+    if (email.value === "" || password.value === "") {
+      return alert("Please fill in all the fields!");
+    }
+    const formData = new FormData(event.currentTarget);
+    formData.forEach((value, name) => {
+        console.log(`${name}: ${value}`);
+        event.currentTarget.reset();
+    })
+};
+
+form.addEventListener("submit", handleSubmit);
