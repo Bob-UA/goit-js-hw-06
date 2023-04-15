52c1dfc12d6f71dfe3f396e4c5aff6f58eeae8be
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-14 21:11:34 -0700

    9-task_completed

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,15 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
+  const randomColor = `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
+    body.style.backgroundColor = `${randomColor}`;
+    color.textContent = `${randomColor}`;
 }
+
+const button = document.querySelector(".change-color");
+const body = document.querySelector("body");
+const color = document.querySelector(".color");
+
+button.addEventListener("click", getRandomHexColor);
+
+
