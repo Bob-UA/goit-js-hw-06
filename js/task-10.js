fc90c55e7a89f1be070c198d69dc09df9b25d3bd
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-15 08:31:54 -0700

    we_get_value_from_input_by_pushing_create-button

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,19 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const input = document.querySelector("input");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");
+const boxes = document.querySelector("#boxes");
+const controls = document.querySelector("#controls");
+
+function createBoxes(amount) {
+  console.log(amount);
+};
+
+function takeValue() {
+  input.addEventListener("input", createBoxes(input.value))
+}
+
+createButton.addEventListener("click", takeValue);
