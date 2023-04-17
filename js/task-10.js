d359597265d7aaa25d890a13e10d734289ba4afc
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-17 13:32:59 -0700

    almost_resolved

--- js/task-10.js
+++ js/task-10.js
@@ -12,10 +12,25 @@ const controls = document.querySelector("#controls");
 
 function createBoxes(amount) {
   console.log(amount);
+  const container = [];
+  for (let i = boxes.childNodes.length + 1; i <= amount; i++) {
+    console.log(i);
+    container.push(`<div style="width: ${20 + 10 * i}px; height: ${20 + 10 * i}px;background-color:${getRandomHexColor()}";></div>`);
+    
+  
+  
+  }
+    boxes.insertAdjacentHTML("beforeend", container.join(""));
+    console.log(boxes);
+  
 };
+
 
 function takeValue() {
   input.addEventListener("input", createBoxes(input.value))
 }
 
 createButton.addEventListener("click", takeValue);
+    
+
+console.log(boxes);
