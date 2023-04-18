0fe04b640b5ecf07e39e04e53526a29eaa6638bd
Bob-UA <120073426+Bob-UA@users.noreply.github.com>
2023-04-18 12:59:18 -0700

    7-task_completed

--- js/task-07.js
+++ js/task-07.js
@@ -1,8 +1,9 @@
 const fontSizeControl = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
+text.style = `font-size:${fontSizeControl.value}px`;
 
 const fontSizeChange = (() => {
     text.style = `font-size:${fontSizeControl.value}px`;
 });
 
-fontSizeControl.addEventListener("click", fontSizeChange);
+fontSizeControl.addEventListener("input", fontSizeChange);
